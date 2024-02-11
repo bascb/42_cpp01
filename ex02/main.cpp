b7fe78de5a8862a9d8ddea3c7fdd8dfceb9b4c25
Bernardo <bercbranco@gmail.com>
2024-02-11 19:34:08 +0000

    ex02 ready to test

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 19:14:27 by bcastelo          #+#    #+#             */
+/*   Updated: 2024/02/11 19:31:34 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main( void )
+{
+	std::string	msg = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &msg;
+	std::string	&stringREF = msg;
+
+	std::cout << "Memory address of the string variable: " << &msg << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << msg << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return 0;
+}
