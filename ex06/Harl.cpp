745285a18c2af47a7a15afd718903524ebccdc99
Bernardo <bercbranco@gmail.com>
2024-02-20 22:33:21 +0000

    Module 1 ready for revision

---
+++ ex06/Harl.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 19:17:28 by bcastelo          #+#    #+#             */
+/*   Updated: 2024/02/20 22:00:44 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::complain( std::string level )
+{
+	func_ptr	functions[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error, &Harl::unknown};
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			i = 0;
+
+	while (i < 4 && levels[i].compare(level))
+		i++;
+	(this->*functions[i])();
+}
+
+void	Harl::debug( void )
+{
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!" << std::endl;
+	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
+}
+
+void	Harl::unknown( void )
+{
+	std::cout << "Unknown log type!" << std::endl;
+}
---
+++ ex06/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 19:09:55 by bcastelo          #+#    #+#             */
+/*   Updated: 2024/02/20 22:30:59 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Harl.hpp"
+
+int	get_level( std::string level );
+
+int	main (int argc, char **argv)
+{
+	Harl	harl;
+	int		level;
+	
+	if (argc != 2)
+	{
+		std::cout << "Usage: " << argv[0] << " <log level>" << std::endl;
+		return 0;
+	}
+	level = get_level(argv[1]);
+	switch (level)
+	{
+		case 0:
+			harl.complain("DEBUG");
+			harl.complain("INFO");
+			harl.complain("WARNING");
+			harl.complain("ERROR");
+			break;
+		case 1:
+			harl.complain("INFO");
+			harl.complain("WARNING");
+			harl.complain("ERROR");
+			break;
+		case 2:
+			harl.complain("WARNING");
+			harl.complain("ERROR");
+			break;
+		case 3:
+			harl.complain("ERROR");
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return (0);
+}
+
+int	get_level( std::string level )
+{
+	int			i;
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	i = 0;
+	while (i < 4 && levels[i].compare(level))
+		i++;
+	return (i);
+}
